19de38c5bd719bd496da261f6d144fbf8052adc8
ashwin47 <thatsashwin@gmail.com>
2016-01-10 03:08:38 +0530

    initial commit

--- app/js/app.js
+++ app/js/app.js
@@ -2,10 +2,7 @@
 
 // Declare app level module which depends on views, and components
 angular.module('myApp', [
-  'ngRoute',
-  'myApp.view1',
-  'myApp.view2',
-  'myApp.version'
+  'ngRoute'
 ]).
 config(['$routeProvider', function($routeProvider) {
   $routeProvider.otherwise({redirectTo: '/view1'});
